95d7ed9c6524569e6645063d9566f14a2fae9135
wudidada <hackerjiang@gmail.com>
2021-12-09 17:36:17 +0800

    version 1.3

--- TicketingDS.java
+++ TicketingDS.java
@@ -3,77 +3,98 @@ package ticketingsystem;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class TicketingDS implements TicketingSystem {
 
     AtomicLong current_tid = new AtomicLong(0);
 
-    List<List<Set<Seat>>> empty_seat_list;
+    List<List<BitSet>> empty_seat_list;
+
+    List<List<Lock>> lock_list;
 
     Map<Long, Ticket> tid_ticket_map = new ConcurrentHashMap<>();
 
-    Map<Long, Seat> tid_seat_map = new ConcurrentHashMap<>();
+    int total_seat_num;
+
+    int seat_num;
+
+    int coach_num;
+
 
     public TicketingDS(int routenum, int coachnum, int seatnum, int stationnum, int threadnum) {
-        List<List<Set<Seat>>> empty_seat_route = new ArrayList<>();
+        List<List<BitSet>> empty_seat_route = new ArrayList<>();
+        List<List<Lock>> lock_route = new ArrayList<>();
         empty_seat_route.add(null);
+        lock_route.add(null);
 
-        List<Seat> seats = new ArrayList<>();
-        for (int coach = 1; coach <= coachnum; coach++) {
-            for (int seat = 1; seat <= seatnum; seat++) {
-                seats.add(new Seat(coach, seat));
-            }
-        }
+        total_seat_num = coachnum * seatnum;
+        coach_num = coachnum;
+        seat_num = seatnum;
 
         for (int route = 1; route <= routenum; route++) {
-            List<Set<Seat>> empty_seat_station = new ArrayList<>();
+            List<BitSet> empty_seat_station = new ArrayList<>();
+            List<Lock> lock_station = new ArrayList<>();
             empty_seat_station.add(null);
+            lock_station.add(null);
             for (int station = 1; station <= stationnum; station++) {
-                Set<Seat> empty_seat = ConcurrentHashMap.newKeySet();
-                empty_seat.addAll(seats);
+                BitSet empty_seat = new BitSet(total_seat_num);
+                empty_seat.set(0, total_seat_num, true);
                 empty_seat_station.add(empty_seat);
+                lock_station.add(new ReentrantLock());
             }
             empty_seat_route.add(empty_seat_station);
+            lock_route.add(lock_station);
         }
         this.empty_seat_list = empty_seat_route;
+        this.lock_list = lock_route;
     }
 
-    private List<Set<Seat>> getRoute(int route) {
-        return this.empty_seat_list.get(route);
+    private List<BitSet> getRoute(int route) {
+        return empty_seat_list.get(route);
     }
 
-    private Set<Seat> getStation(int route, int station) {
-        return this.empty_seat_list.get(route).get(station);
-    }
+    private BitSet get_empty_seat(int route, int departure, int arrival) {
+        List<BitSet> empty_seat_list = getRoute(route);
+        BitSet empty_seat = (BitSet) empty_seat_list.get(departure).clone();
 
-    private Set<Seat> get_empty_seat(int route, int departure, int arrival) {
-        List<Set<Seat>> empty_seat_list = getRoute(route);
-        Set<Seat> empty_seat = new TreeSet<>(empty_seat_list.get(departure));
-        for (int i = departure + 1; !empty_seat.isEmpty() && i < arrival; i++) {
-            empty_seat.retainAll(empty_seat_list.get(i));
+        for (int i = departure + 1;  empty_seat.cardinality() >0 && i < arrival; i++) {
+            empty_seat.and(empty_seat_list.get(i));
         }
         return empty_seat;
     }
 
     public Ticket buyTicket(String passenger, int route, int departure, int arrival) {
-        Set<Seat> empty_seat = get_empty_seat(route, departure, arrival);
-        if (empty_seat.size() == 0)
+        BitSet empty_seat = get_empty_seat(route, departure, arrival);
+        if (empty_seat.cardinality() == 0)
             return null;
 
-        Seat my_seat = ((TreeSet<Seat>) empty_seat).first();
+        int seat_index = get_first_seat(empty_seat);
 
-        List<Set<Seat>> station_empty_seats = getRoute(route);
+        List<BitSet> station_empty_seats = getRoute(route);
+        List<Lock> route_lock = lock_list.get(route);
+
         int station = departure;
         boolean success = true;
 
-        while (station < arrival && success) {
-            success = station_empty_seats.get(station).remove(my_seat);
+        while (station < arrival) {
+            route_lock.get(station).lock();
+            if (!station_empty_seats.get(station).get(seat_index)) {
+                route_lock.get(station).unlock();
+                success = false;
+                break;
+            }
+            station_empty_seats.get(station).set(seat_index, false);
+            route_lock.get(station).unlock();
             station += 1;
         }
 
         if (!success) {
             for(int i = departure; i < station; i++) {
-                station_empty_seats.get(i).add(my_seat);
+                route_lock.get(i).lock();
+                station_empty_seats.get(i).set(seat_index, true);
+                route_lock.get(i).unlock();
             }
             return buyTicket(passenger, route, departure, arrival);
         }
@@ -81,22 +102,42 @@ public class TicketingDS implements TicketingSystem {
         long tid = current_tid.addAndGet(1);
         Ticket ticket = new Ticket();
         ticket.tid = tid;
-        ticket.coach = my_seat.coach;
-        ticket.seat = my_seat.seat;
+        ticket.coach = index_to_coach(seat_index);
+        ticket.seat = index_to_seat(seat_index);
         ticket.arrival = arrival;
         ticket.departure = departure;
         ticket.passenger = passenger;
         ticket.route = route;
 
         tid_ticket_map.put(tid, ticket);
-        tid_seat_map.put(tid, my_seat);
 
         return ticket;
     }
 
+    private int index_to_coach(int seat_index) {
+        return seat_index / seat_num + 1;
+    }
+
+    private int index_to_seat(int seat_index) {
+        return seat_index % seat_num + 1;
+    }
+
+
+    private int get_first_seat(BitSet empty_seats) {
+        for (int i = 0; i < total_seat_num; i++) {
+            if (empty_seats.get(i)) {
+                return i;
+            }
+        }
+
+        System.err.println("get empty seat failed.");
+
+        return -1;
+    }
+
     // 返回余票数
     public int inquiry(int route, int departure, int arrival) {
-        return get_empty_seat(route, departure, arrival).size();
+        return get_empty_seat(route, departure, arrival).cardinality();
     }
 
     @Override
@@ -125,14 +166,21 @@ public class TicketingDS implements TicketingSystem {
             return false;
         }
 
-        Seat seat = tid_seat_map.remove(sys_ticket.tid);
+        int seat_index = get_seat_index(sys_ticket.coach, sys_ticket.seat);
 
-        List<Set<Seat>> station_seat = getRoute(sys_ticket.route);
+        List<BitSet> station_seat = getRoute(sys_ticket.route);
+        List<Lock> station_lock = lock_list.get(sys_ticket.route);
         for (int station = sys_ticket.departure; station < sys_ticket.arrival; station++) {
-            station_seat.get(station).add(seat);
+            station_lock.get(station).lock();
+            station_seat.get(station).set(seat_index, true);
+            station_lock.get(station).unlock();
         }
 
         return true;
+    }
+
+    private int get_seat_index(int coach, int seat) {
+        return (coach - 1) * seat_num + seat - 1;
     }
 
     public static void main(String[] args) {
